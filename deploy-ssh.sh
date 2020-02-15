ab41021a815ea99b62d69593f174a43358a6fcf5
Andrey <geass1000@gmail.com>
2020-02-16 02:46:54 +0300

    feat(ci): create images folder

--- deploy-ssh.sh
+++ deploy-ssh.sh
@@ -2,5 +2,8 @@
 
 whereSaveImages=$1
 
+echo "Remove ${whereSaveImages}"
 ssh $SSH_USER@$SSH_HOST "rm -r ${whereSaveImages} && mkdir ${whereSaveImages}"
+
+echo "Copy ${whereSaveImages}"
 scp -r "./${whereSaveImages}/" $SSH_USER@$SSH_HOST:~/
