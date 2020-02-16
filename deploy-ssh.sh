a6d99d94eaf15c7cadb093b8cb4346a19a6d3077
Andrey <geass1000@gmail.com>
2020-02-16 03:41:42 +0300

    feat(ci): get folder from dist

--- deploy-ssh.sh
+++ deploy-ssh.sh
@@ -6,4 +6,4 @@ echo "Remove ${whereSaveImages}"
 ssh $SSH_USER@$SSH_HOST "rm -r ${whereSaveImages} && mkdir ${whereSaveImages}"
 
 echo "Copy ${whereSaveImages}"
-scp -r "./${whereSaveImages}/" $SSH_USER@$SSH_HOST:~/
+scp -r "./dist/${whereSaveImages}/" $SSH_USER@$SSH_HOST:~/
